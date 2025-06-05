409e3b8b09aefe0efc910a31ba6167e7beafc841
theportinhol123 <theportinhol@gmail.com>
2025-06-05 10:26:30 +0100

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,8 +1,11 @@
 let pulse = 0
 let totalLitros = 0
 
+/**
+ * Iniciar o sensor de fluxo no pino P0
+ */
+//% block="iniciar sensor de fluxo no pino P0"
 //% group="Água"
-//% block="Iniciar sensor de fluxo no pino P0"
 export function iniciarSensorFluxo(): void {
     pulse = 0
     totalLitros = 0
@@ -11,21 +14,30 @@ export function iniciarSensorFluxo(): void {
     })
 }
 
+/**
+ * Atualizar litros (deve ser chamado a cada segundo)
+ */
+//% block="atualizar litros a cada segundo"
 //% group="Água"
-//% block="Atualizar litros (a cada segundo)"
 export function atualizarLitros(): void {
     totalLitros += pulse / 7.5 / 60
     pulse = 0
 }
 
+/**
+ * Mostrar no ecrã o total arredondado de litros
+ */
+//% block="mostrar total de litros arredondado"
 //% group="Água"
-//% block="Mostrar total de litros arredondado"
 export function mostrarLitros(): void {
     basic.showNumber(Math.round(totalLitros))
 }
 
+/**
+ * Obter o valor total de litros medidos
+ */
+//% block="obter total de litros"
 //% group="Água"
-//% block="Obter total de litros"
 export function obterLitros(): number {
     return totalLitros
 }
